82166e2acca570d9d2a0d9b01add556757c8330f
karinacrmcldev <perfechate@gmail.com>
2021-08-25 19:40:59 +0300

    Moved files to different folder

---
+++ src/components/posts-content/news/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../../post/header";
+import Actions from "../../post/actions";
+import Content from "./content";
+import Hot from "../is-hot";
+
+export default function PostNews({ object, isHot }) {
+  return (
+    <div className="mb-14 relative lptpXS:mb-8 bg-white w-post lptpXL:w-postMd lptpXS:w-postSm rounded-lg shadow-xl mobileXL:shadow-none mobileXL:w-full">
+      <Header object={object} />
+      {isHot ? <Hot /> : null}
+      <Content object={object} />
+      <Actions
+        object={object}
+        totalLikes={object.likes}
+        publicationDocId={object.docId}
+        likedPublication={object.userLikedPublication}
+        pinnedPublication={object.userPinnedPublication}
+      />
+    </div>
+  );
+}
---
+++ src/components/posts-content/photography/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../../post/header";
+import Actions from "../../post/actions";
+import Hot from "../is-hot";
+
+export default function PostPhotography({ object, isHot }) {
+  return (
+    <div className="mb-14 relative bg-white lptpXS:mb-8 w-post lptpXL:w-postMd lptpXS:w-postSm rounded-lg shadow-xl mobileXL:shadow-none mobileXL:w-full">
+      <Header object={object} /> {isHot ? <Hot /> : null}
+      <div className="w-full max-h-post  overflow-hidden">
+        <img src={object.photo} className="w-full object-cover" />
+      </div>
+      <Actions
+        object={object}
+        totalLikes={object.likes}
+        publicationDocId={object.docId}
+        likedPublication={object.userLikedPublication}
+        pinnedPublication={object.userPinnedPublication}
+      />
+    </div>
+  );
+}
