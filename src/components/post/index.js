ee15379c6c6bcb3010b936cf395746315f968ce7
karinacrmcldev <perfechate@gmail.com>
2021-08-29 20:23:00 +0300

    Changed rendering method, refactored

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,8 +2,9 @@ import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Actions from "./actions";
+import Hot from "./is-hot";
 
-export default function Post({ children, content }) {
+export default function PostLayout({ children, isHot, object }) {
   return (
     <div
       className="font-fontbasic relative bg-white w-post lptpXS:mb-8 lptpXL:w-postMd lptpXS:w-postSm rounded-lg shadow-xl mb-14 mobileXL:shadow-none mobileXL:w-full "
