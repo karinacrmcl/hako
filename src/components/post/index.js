6821204388d89b87d7f6ac81ab69e0e8f6d888d5
karinacrmcldev <perfechate@gmail.com>
2021-08-29 19:50:47 +0300

    Post layout cleanup

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,14 +1,9 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
-import Image from "./image";
 import Actions from "./actions";
-import Footer from "./footer";
-import Comments from "./comments";
 
-export default function Post({ content }) {
-  const commentInput = useRef(null);
-  const handleFocus = () => commentInput.current.focus();
+export default function Post({ children, content }) {
   return (
     <div
       className="font-fontbasic relative bg-white w-post lptpXS:mb-8 lptpXL:w-postMd lptpXS:w-postSm rounded-lg shadow-xl mb-14 mobileXL:shadow-none mobileXL:w-full "
@@ -16,8 +11,7 @@ export default function Post({ content }) {
     >
       <Header object={object} />
       {isHot ? <Hot /> : null}
-
-      <Image src={content.imageSrc} caption={content.caption} />
+      {children}
       <Actions
         object={object}
         publicationDocId={object.docId}
@@ -28,15 +22,15 @@ export default function Post({ content }) {
   );
 }
 
-Post.propTypes = {
-  content: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    imageSrc: PropTypes.string.isRequired,
-    caption: PropTypes.string.isRequired,
-    docId: PropTypes.string.isRequired,
-    userLikedPhoto: PropTypes.bool.isRequired,
-    likes: PropTypes.array.isRequired,
-    comments: PropTypes.array.isRequired,
-    dateCreated: PropTypes.number.isRequired,
-  }),
-};
+// Post.propTypes = {
+//   content: PropTypes.shape({
+// username: PropTypes.string.isRequired,
+// imageSrc: PropTypes.string.isRequired,
+// caption: PropTypes.string.isRequired,
+// docId: PropTypes.string.isRequired,
+// userLikedPhoto: PropTypes.bool.isRequired,
+// likes: PropTypes.array.isRequired,
+// comments: PropTypes.array.isRequired,
+// dateCreated: PropTypes.number.isRequired,
+//   }),
+// };
