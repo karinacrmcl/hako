ede4101a5e3fc22677ed01ef7d96c19d138c4e6e
karinacrmcldev <perfechate@gmail.com>
2021-09-11 18:31:29 +0300

    Fixed imports

--- src/components/add-posts/news/content.js
+++ src/components/add-posts/news/content.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { addPublication } from "../../../services/firebase";
 import PublicationSuccess from "../publication-success/";
-import ButtonSecond from "../../../shared/button-2";
 import Loader from "react-loader-spinner";
 import { useMediaQuery } from "react-responsive";
 import Skeleton from "react-loading-skeleton";
+import ButtonFilled from "../../../shared/components/button-filled";
 
 export default function Content({ user }) {
   //inputs with the text
@@ -349,7 +349,7 @@ export default function Content({ user }) {
         className="absolute right-0 -bottom-10 tabletXL:-top-12 tabletXL:bottom-auto mobileXL:-top-9"
         onClick={(e) => savePostData(e)}
       >
-        <ButtonSecond />
+        <ButtonFilled />
       </div>
       <PublicationSuccess isPublished={isPublished} />
     </div>
