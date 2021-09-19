9f56079e3f70defc19b212e080bc4fe71d6bc134
karinacrmcldev <perfechate@gmail.com>
2021-09-19 21:25:06 +0300

    Refactoring

--- src/components/category-sorting/index.js
+++ src/components/category-sorting/index.js
@@ -1,52 +1,13 @@
-import { useEffect, useState } from "react";
-import Item from "./item";
-import Hint from "./hint";
+import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
-export default function Sorting({ isOnTop }) {
-  const categories = [
-    {
-      title: "Hot / Trending",
-      type: "hot",
-      icon: "hot.svg",
-      id: "1",
-    },
-    {
-      title: "Latest News",
-      type: "news",
-      icon: "news.svg",
-      id: "2",
-    },
-    {
-      title: "Photography ",
-      type: "photography",
-      icon: "photo.svg",
-      id: "3",
-    },
-    {
-      title: "Posts & Articles",
-      type: "article",
-      icon: "post.svg",
-      id: "4",
-    },
-    {
-      title: "Books recomendations",
-      type: "book",
-      icon: "book.svg",
-      id: "5",
-    },
-    {
-      title: "Discussions",
-      type: "discussion",
-      icon: "discussion.svg",
-      id: "6",
-    },
-  ];
+import Item from "./item";
+import Hint from "./hint";
+import { categoriesSorting } from "../../constants/categories";
 
+export default function Sorting({ isOnTop }) {
   const [categoriesHovered, setCategoriesHovered] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: "650px" });
-
-  const [offset, setOffset] = useState(null);
 
   return (
     <div
@@ -57,7 +18,7 @@ export default function Sorting({ isOnTop }) {
       <div
         className={`flex flex-col overflow-hidden transition-all duration-400 ease-in-out bg-white items-center shadow-xl rounded-lg p-3 lptpXS:p-2 mobileXL:shadow-none mobileXL:w-3/4 mobileXL:p-1 mobileXL:h-10 mobileXL:flex-row mobileXL:items-center   ${
           categoriesHovered ? "w-56" : "w-16 lptpXS:w-12 "
-        } ${offset == "up" ? "h-10" : "opacitty-0"}`}
+        } `}
         onMouseEnter={() => {
           if (isMobile) {
             return;
@@ -77,7 +38,7 @@ export default function Sorting({ isOnTop }) {
           {isMobile ? null : "SORT"}
         </h3>
         <div className="flex self-start flex-col mobileXL:flex-row  mobileXL:items-center mobileXL:h-full  ">
-          {categories.map((item, i) => (
+          {categoriesSorting.map((item, i) => (
             <Item
               item={item}
               categoriesHovered={categoriesHovered}
