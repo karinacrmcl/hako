10ccd9aa8d2b8c64576acbf874aa3d3a38bb5e1d
karinacrmcldev <perfechate@gmail.com>
2021-09-27 23:10:46 +0300

    Timeline refactoring

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -13,6 +13,56 @@ export default function Timeline() {
   const { publications } = usePhotos();
   const { activeCategories } = useCategories();
 
+  function getContent(content) {
+    switch (content.type) {
+      case "article":
+        return (
+          activeCategories.article && (
+            <PostLayout object={content} isHot={content.isHot}>
+              <ArticleContent object={content} key={content.id} />
+            </PostLayout>
+          )
+        );
+      case "book":
+        return (
+          activeCategories.book && (
+            <PostLayout object={content} isHot={content.isHot}>
+              <BookContent object={content} key={content.id} />
+            </PostLayout>
+          )
+        );
+      case "news":
+        return (
+          activeCategories.news && (
+            <PostLayout object={content} isHot={content.isHot}>
+              <NewsContent object={content} key={content.id} />
+            </PostLayout>
+          )
+        );
+      case "discussion":
+        return (
+          activeCategories.discussion && (
+            <PostLayout object={content} isHot={content.isHot}>
+              <DiscussionContent object={content} key={content.id} />
+            </PostLayout>
+          )
+        );
+      case "photography":
+        return (
+          activeCategories.photography && (
+            <PostLayout object={content} isHot={content.isHot}>
+              <PhotographyContent object={content} key={content.id} />
+            </PostLayout>
+          )
+        );
+
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div
       className="overflow-y-scroll max-h-screen py-20 px-2 -mt-20 mobileXL:-mt-18 mobileXL:w-full mobileXL:px-2 mobileXL:overflow-x-hidden"
@@ -29,53 +79,7 @@ export default function Timeline() {
         </>
       ) : publications?.length > 0 ? (
         publications.map((content) => {
-          switch (content.type) {
-            case "article":
-              return (
-                activeCategories.article && (
-                  <PostLayout object={content} isHot={content.isHot}>
-                    <ArticleContent object={content} key={content.id} />
-                  </PostLayout>
-                )
-              );
-            case "book":
-              return (
-                activeCategories.book && (
-                  <PostLayout object={content} isHot={content.isHot}>
-                    <BookContent object={content} key={content.id} />
-                  </PostLayout>
-                )
-              );
-            case "news":
-              return (
-                activeCategories.news && (
-                  <PostLayout object={content} isHot={content.isHot}>
-                    <NewsContent object={content} key={content.id} />
-                  </PostLayout>
-                )
-              );
-            case "discussion":
-              return (
-                activeCategories.discussion && (
-                  <PostLayout object={content} isHot={content.isHot}>
-                    <DiscussionContent object={content} key={content.id} />
-                  </PostLayout>
-                )
-              );
-            case "photography":
-              return (
-                activeCategories.photography && (
-                  <PostLayout object={content} isHot={content.isHot}>
-                    <PhotographyContent object={content} key={content.id} />
-                  </PostLayout>
-                )
-              );
-
-              break;
-
-            default:
-              break;
-          }
+          return <div key={content.id}>{getContent(content)}</div>;
         })
       ) : (
         <p className="text-center text-2xl">Follow more people to see posts</p>
