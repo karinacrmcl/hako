a2c92641b3092a390ba6015f21745d812ebda72a
karinacrmcldev <perfechate@gmail.com>
2021-09-27 21:26:43 +0300

    Refactoring

---
+++ src/constants/categories.js
@@ -0,0 +1,124 @@
+export const categoriesSorting = [
+  {
+    title: "Hot / Trending",
+    type: "hot",
+    icon: "hot",
+    id: "1",
+  },
+  {
+    title: "Latest News",
+    type: "news",
+    icon: "news",
+    id: "2",
+  },
+  {
+    title: "Photography ",
+    type: "photography",
+    icon: "photo",
+    id: "3",
+  },
+  {
+    title: "Posts & Articles",
+    type: "article",
+    icon: "post",
+    id: "4",
+  },
+  {
+    title: "Books recomendations",
+    type: "book",
+    icon: "book",
+    id: "5",
+  },
+  {
+    title: "Discussions",
+    type: "discussion",
+    icon: "discussion",
+    id: "6",
+  },
+];
+
+export const addCategories = [
+  {
+    title: "news",
+    svgUrl: "news",
+    position: "-left-10 top-0",
+    id: 1,
+    animDuration: 100,
+    expl: "News Post",
+    key: "modalAddNews",
+  },
+  {
+    title: "photography",
+    svgUrl: "photography",
+    position: "-left-6 -top-7",
+    id: 2,
+    animDuration: 200,
+    expl: "Photography",
+    key: "modalAddPhoto",
+  },
+  {
+    title: "article",
+    svgUrl: "article",
+    position: "-top-10 left-2.5",
+    id: 3,
+    animDuration: 300,
+    expl: "Article",
+    key: "modalAddArticle",
+  },
+  {
+    title: "book",
+    svgUrl: "book",
+    position: "-right-3 -top-7",
+    id: 4,
+    animDuration: 400,
+    expl: "Book",
+    key: "modalAddBook",
+  },
+  {
+    title: "discussion",
+    svgUrl: "discussion",
+    position: "-right-8 top-0",
+    id: 5,
+    animDuration: 500,
+    expl: "Discussion",
+    key: "modalAddDiscussion",
+  },
+];
+
+export const addCategoriesMobile = [
+  {
+    title: "Post / Article",
+    icon: "article-mobile",
+    index: "1",
+    modalKey: "modalAddArticle",
+    animDuration: 100,
+  },
+  {
+    title: "News post",
+    icon: "news-mobile",
+    index: "2",
+    modalKey: "modalAddNews",
+    animDuration: 200,
+  },
+  {
+    title: "Photography",
+    icon: "photography-mobile",
+    index: "3",
+    modalKey: "modalAddPhoto",
+    animDuration: 300,
+  },
+  {
+    title: "Book post",
+    icon: "book-mobile",
+    index: "4",
+    modalKey: "modalAddBook",
+    animDuration: 400,
+  },
+  {
+    title: "Discussion",
+    icon: "discussion-mobile",
+    index: "5",
+    modalKey: "modalAddDiscussion",
+    animDuration: 500,
+  },
+];
