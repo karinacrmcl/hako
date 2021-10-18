1453b4e4d7161bcab3af08beaee91500307b4937
karinacrmcldev <perfechate@gmail.com>
2021-10-18 16:18:23 +0300

    Fixes in functions

--- src/components/profile/follows/list-item.js
+++ src/components/profile/follows/list-item.js
@@ -3,26 +3,24 @@ import { Link } from "react-router-dom";
 export function SingleUserInfo({ user }) {
   return (
     <>
-      {user ? (
-        <div className="flex justify-between mt-2 hover:bg-gray-background cursor-pointer px-4 py-1">
-          <Link
-            to={`/p/${user ? user.username : null}`}
-            className="flex items-center"
-          >
-            <div className="flex items-center">
-              <img
-                src={
-                  user
-                    ? user.avatarUrl.min
-                    : "https://i.ibb.co/ZY5mytK/user-deleted.png"
-                }
-                className="avatar-list rounded-full object-cover"
-              />
-              <p className="text-sm font-medium ml-2">{user.username}</p>
-            </div>
-          </Link>
-        </div>
-      ) : null}
+      {user && (
+        <Link
+          to={`/p/${user ? user.username : null}`}
+          className="flex justify-between mt-2 hover:bg-gray-background cursor-pointer px-4 py-1"
+        >
+          <div className="flex items-center">
+            <img
+              src={
+                user
+                  ? user.avatarUrl.min
+                  : "https://i.ibb.co/ZY5mytK/user-deleted.png"
+              }
+              className="avatar-list rounded-full object-cover"
+            />
+            <p className="text-sm font-medium ml-2">{user.username}</p>
+          </div>
+        </Link>
+      )}
     </>
   );
 }
