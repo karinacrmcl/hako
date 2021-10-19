2c57eaee9a32f5d6dfe10b8ad2a5cdcb2f3e740d
karinacrmcldev <perfechate@gmail.com>
2021-10-19 16:19:06 +0300

    Component refactoring

---
+++ src/constants/profileCategories.js
@@ -0,0 +1,22 @@
+export const categoriesList = [
+  {
+    label: "photography",
+    id: 1,
+  },
+  {
+    label: "news",
+    id: 2,
+  },
+  {
+    label: "article",
+    id: 3,
+  },
+  {
+    label: "book",
+    id: 4,
+  },
+  {
+    label: "discussion",
+    id: 5,
+  },
+];
