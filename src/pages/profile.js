5bc501801f101f7ce68b09efa09fcf9e60748899
karinacrmcldev <perfechate@gmail.com>
2021-10-20 16:19:12 +0300

    Component refactoring

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -27,7 +27,7 @@ export default function Profile() {
   return user?.username ? (
     <div className="font-fontbasic mobileXL:bg-gray-bgMobile">
       <Header />
-      <div className="flex justify-center w-full">
+      <div className="flex justify-center w-full h-full">
         <div className="mt-24 py-2 flex justify-between mt-32 lptpXS:mt-28 w-full max-w-containerLg lptpXL:px-6 lptpXS:px-8 mobileSM:px-2.5 rselative mobileXL:mt-16 mobileXL:flex-col mobileXL:items-center tabletXL:mt-20">
           <UserProfile profileUser={user} />
         </div>
