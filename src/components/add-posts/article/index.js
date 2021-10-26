795ee4eecca94ebd1bcf836bce5e2f3081c30d0f
karinacrmcldev <perfechate@gmail.com>
2021-10-26 21:48:47 +0300

    Refactoring

--- src/components/add-posts/article/index.js
+++ src/components/add-posts/article/index.js
@@ -1,15 +1,83 @@
-import React from "react";
-import Base from "../base-modal";
-import Content from "./content";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { dateOptions } from "../../../constants/date-options";
+import { addPublication } from "../../../services/firebase";
+import Base from "../layout";
+import PublicationSuccess from "../publication-success/";
 
 export default function AddArticle({ user }) {
+  const [isPublished, setIsPublished] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const post = {
+      category: "Posts & Articles",
+      dateCreate: new Date().toLocaleDateString("en-US", dateOptions),
+      id: new Date().getTime().toString(),
+      title: data.title,
+      text: data.text,
+      type: "article",
+      userId: user.uid,
+      comments: [],
+    };
+    reset();
+    setIsPublished(true);
+    setTimeout(() => {
+      setIsPublished(false);
+    }, 3000);
+    addPublication(post);
+  };
+
   return (
     <>
       <Base
         category="Article"
-        content={<Content user={user} />}
         categoryKey="modalAddArticle"
-      ></Base>
+        onPostHandler={handleSubmit(onSubmit)}
+      >
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex-col flex h-full py-10 relative lptpXL:py-5 lptpXL:w-full"
+        >
+          <input
+            className={`input-add ${errors.title && "input-error"}`}
+            placeholder="Title of the post"
+            type="text"
+            id="title"
+            name="title"
+            {...register("title", {
+              validate: (value) => value.length > 20 && value.length < 120,
+            })}
+          />
+          {errors.title && (
+            <p className="text-sm ml-2 mt-2 text-gray-light">
+              Your title is too short or too long
+            </p>
+          )}
+
+          <textarea
+            className={`textarea-add mt-6 h-96 ${
+              errors.title && "textarea-error"
+            }`}
+            placeholder="What’s on your mind?"
+            {...register("text", {
+              validate: (value) => value.length > 100 && value.length < 1500,
+            })}
+          ></textarea>
+          {errors.text && (
+            <p className="text-sm ml-2 mt-2 text-gray-light">
+              Text should be at least 100 characters long
+            </p>
+          )}
+        </form>
+        <PublicationSuccess isPublished={isPublished} />
+      </Base>
     </>
   );
 }
