0fc4beb3558de28553fb6e0195f4eb54153f2259
karinacrmcldev <perfechate@gmail.com>
2021-10-30 21:49:42 +0300

    Refactoring

--- src/components/add-posts/publication-success/index.js
+++ src/components/add-posts/publication-success/index.js
@@ -1,4 +1,5 @@
 import { Transition } from "react-transition-group";
+import SvgSelector from "../svg-selector";
 
 export default function PublicationSuccess({ isPublished }) {
   const duration = 300;
@@ -26,10 +27,8 @@ export default function PublicationSuccess({ isPublished }) {
           }}
           className="select-none absolute bg-gradient-to-r from-gradient-from to-gradient-to success-container rounded-lg flex items-center p-6 top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"
         >
-          <img
-            src="\images\icons\add-posts\success.svg"
-            className="lptpXL:w-20"
-          />
+          <SvgSelector id="success" className="lptpXL:w-20" />
+
           <div className="flex flex-col ml-3">
             <h2 className="text-white font-bold text-xl uppercase leading-none lptpXS:text-lg	mobileXL:text-base mobileXS:text-sm">
               Your publication was successfully added.
