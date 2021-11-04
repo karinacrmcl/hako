f68f472bb65a32c9c2a06db73d3d843a7845f5db
karinacrmcldev <perfechate@gmail.com>
2021-11-04 20:51:36 +0200

    Refactoring

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -22,16 +22,3 @@ export default function PostLayout({ children, isHot, object }) {
     </div>
   );
 }
-
-// Post.propTypes = {
-//   content: PropTypes.shape({
-// username: PropTypes.string.isRequired,
-// imageSrc: PropTypes.string.isRequired,
-// caption: PropTypes.string.isRequired,
-// docId: PropTypes.string.isRequired,
-// userLikedPhoto: PropTypes.bool.isRequired,
-// likes: PropTypes.array.isRequired,
-// comments: PropTypes.array.isRequired,
-// dateCreated: PropTypes.number.isRequired,
-//   }),
-// };
