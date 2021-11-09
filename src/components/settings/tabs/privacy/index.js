b151a16f14331c1001369845bd01c960752811e3
karinacrmcldev <perfechate@gmail.com>
2021-11-09 20:55:50 +0200

    Refactoring

---
+++ src/components/settings/tabs/privacy/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Privacy() {
+  return (
+    <div className="flex flex-col ml-4 w-contentSettings h-full">
+      <h2 className="text-xl font-medium lptpXS:text-lg">Privacy</h2>
+    </div>
+  );
+}
