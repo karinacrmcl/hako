680d0bb4e7b9222481c503528fc1a0193268f7b8
karinacrmcldev <perfechate@gmail.com>
2021-11-18 20:54:47 +0200

    Refactoring

---
+++ src/components/settings/tabs/about/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function About() {
+  return (
+    <div className="flex flex-col ml-4 w-contentSettings h-full">
+      <h2 className="text-xl font-medium lptpXS:text-lg">About</h2>
+    </div>
+  );
+}
