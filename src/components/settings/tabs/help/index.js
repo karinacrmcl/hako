fe2930065ce0882e3298f611d5d9469811916d95
karinacrmcldev <perfechate@gmail.com>
2021-11-19 20:54:57 +0200

    Refactoring

---
+++ src/components/settings/tabs/help/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Help() {
+  return (
+    <div className="flex flex-col ml-4 w-contentSettings h-full">
+      <h2 className="text-xl font-medium lptpXS:text-lg">Help</h2>
+    </div>
+  );
+}
