592539a0cf2c63ff7d26a31d5d58bfa2370a4731
karinacrmcldev <perfechate@gmail.com>
2021-12-01 20:57:58 +0200

    Refactoring

--- src/context/user-profile-categories.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const ProfileUserCategoriesContext = createContext({
-  setProfileActiveCategories: () => null,
-  profileActiveCategories: "all",
-});
