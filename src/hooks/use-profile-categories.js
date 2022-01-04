9b967508c1c87f4290d782a9715e03344f9534a6
karinacrmcldev <perfechate@gmail.com>
2022-01-04 20:59:02 +0200

    Refactoring

--- src/hooks/use-profile-categories.js
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import { ProfileUserCategoriesContext } from "../context/user-profile-categories";
-
-export const useProfileCategories = () =>
-  useContext(ProfileUserCategoriesContext);
