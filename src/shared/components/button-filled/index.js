7076658fd499e68fc5f5ee87774b56b2049047dd
karinacrmcldev <perfechate@gmail.com>
2022-01-19 20:59:59 +0200

    Refactoring

--- src/shared/components/button-filled/index.js
+++ src/shared/components/button-filled/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function ButtonFilled({ func }) {
+export default function ButtonFilled({ children, func }) {
   return (
     <button
       onClick={func}
-      className="bg-gradient-to-r p-1 px-4 demoGradient text-base font-semibold text-white rounded-lg transition-all lptpXL:text-base lptpXL:py-1.5 lptpXL:mb-1 tabletXL:rounded-xl tabletXL:py-1 tabletXL:text-sm mobileXL:text-xs"
+      className="bg-gradient-to-r h-9 p-1 px-4 demoGradient text-base font-semibold text-white rounded-lg transition-all lptpXL:text-base lptpXL:py-1.5 lptpXL:mb-1 tabletXL:rounded-xl tabletXL:py-1 tabletXL:text-sm mobileXL:text-xs"
     >
-      Post
+      {children}
     </button>
   );
 }
