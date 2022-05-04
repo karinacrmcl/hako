3d7a49c2a48124e687d5f5811c7a9ddc03285871
karinacrmcldev <perfechate@gmail.com>
2022-05-04 14:37:58 +0300

    Fixed server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import path from "path";
-
+const path = require("path");
+const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, "..", "public");
