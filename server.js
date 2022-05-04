034ab58e42c9f38f5f0c32d6679e748cec5ea590
karinacrmcldev <perfechate@gmail.com>
2022-05-04 19:50:15 +0300

    Fixed scripts

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, "..", "build");
+const publicPath = path.join(__dirname, "..", "..", "build");
 
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
+console.log(publicPath, "index/html");
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
